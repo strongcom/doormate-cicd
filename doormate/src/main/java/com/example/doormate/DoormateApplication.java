6dc095b6e1280b66a182b3eea8c5e265d4ccef40
jum0624 <jum0624@naver.com>
2023-01-08 14:33:48 +0900

    Init upload

---
+++ doormate/src/main/java/com/example/doormate/DoormateApplication.java
@@ -0,0 +1,13 @@
+package com.example.doormate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoormateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoormateApplication.class, args);
+	}
+
+}
---
+++ doormate/src/test/java/com/example/doormate/DoormateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doormate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoormateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
