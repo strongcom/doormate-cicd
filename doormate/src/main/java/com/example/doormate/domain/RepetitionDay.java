5b88efca0af3dfc9c16e6ef970ffc983d935ffed
jum0624 <jum0624@naver.com>
2023-01-28 03:08:15 +0900

    fix: RepetitionDay enum key-value로 형식 변경

--- doormate/src/main/java/com/example/doormate/domain/RepetitionDay.java
+++ doormate/src/main/java/com/example/doormate/domain/RepetitionDay.java
@@ -1,5 +1,27 @@
 package com.example.doormate.domain;
 
 public enum RepetitionDay {
-    SUN, MON, TUE, WED, THUR, FRI, SAT;
+    MON(1, "월요일"),
+    TUE(2, "화요일"),
+    WED(3, "수요일"),
+    THUR(4, "목요일"),
+    FRI(5, "금요일"),
+    SAT(6, "토요일"),
+    SUN(7, "일요일");
+
+    private int number;
+    private String day;
+
+    RepetitionDay(int number, String day) {
+        this.number = number;
+        this.day = day;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDay() {
+        return day;
+    }
 }
